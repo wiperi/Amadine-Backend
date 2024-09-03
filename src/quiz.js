d5e26050e83ab5ac86244a1bf8c2702447054890
cheong Zhang <z5512751@ad.unsw.edu.au>
2024-09-03 19:17:30 +1000

    chore: Restyle the function adminQuizInfo

--- src/quiz.js
+++ src/quiz.js
@@ -5,10 +5,11 @@ function adminQuizDescriptionUpdate (authUserId, quizId, description) {
 
 //Get all of the relevant information about the current quiz.
 function adminQuizInfo(authUserId, quizId){
-    return{ quizId: 1, 
-      name: 'My Quiz', 
-      timeCreated: 1683125870, 
-      timeLastEdited: 1683125871, 
-      description: 'This is my quiz', 
-    };
+  return{ 
+    quizId: 1, 
+    name: 'My Quiz', 
+    timeCreated: 1683125870, 
+    timeLastEdited: 1683125871, 
+    description: 'This is my quiz', 
+  };
 }
