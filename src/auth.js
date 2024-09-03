c868dc0f3e17de36f92e80f6514a23f0bbe3dbea
Yuting <z5535043@ad.unsw.edu.au>
2024-09-04 06:38:08 +1000

    chore: add new function adminUserDetails framework

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,24 @@
 function adminAuthRegister ( email, password, nameFirst, nameLast ) {
-    return {
-        authUserId: 1,
-    };
+  return {
+    authUserId: 1,
+  };
 }
 
 function adminAuthLogin ( email, password ) {
-    return {
-        authUserId: 1,
-    };
+  return {
+    authUserId: 1,
+  };
+}
+
+function adminUserDetails ( authUserId ) {
+  return {
+    user:
+    {
+      userId: 1,
+      name: 'Jar Jar Brinks',
+      email: 'mesasosorry@naboo.com.au',
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 1,
+    }
+  };
 }
