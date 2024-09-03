e4e0db2ce12445735bf677c3eb453a972ae58697
Yuting <z5535043@ad.unsw.edu.au>
2024-09-03 18:13:36 +1000

    chore: add function adminAuthLogin framework

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,11 @@
 function adminAuthRegister ( email, password, nameFirst, nameLast ) {
     return {
         authUserId: 1,
-    }
+    };
+}
+
+function adminAuthLogin ( email, password ) {
+    return {
+        authUserId: 1,
+    };
 }
