3c306dc2ec0cb1be2f4f061cec5473f2405325e0
Cheong <z5512751@unsw.edu.au>
2024-09-03 19:20:12 +1000

    chore: add function adminQuizName update to quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -5,10 +5,16 @@ function adminQuizDescriptionUpdate (authUserId, quizId, description) {
 
 //Get all of the relevant information about the current quiz.
 function adminQuizInfo(authUserId, quizId){
-    return{ quizId: 1, 
-      name: 'My Quiz', 
-      timeCreated: 1683125870, 
-      timeLastEdited: 1683125871, 
-      description: 'This is my quiz', 
-    };
+  return{ 
+    quizId: 1, 
+    name: 'My Quiz', 
+    timeCreated: 1683125870, 
+    timeLastEdited: 1683125871, 
+    description: 'This is my quiz', 
+  };
+}
+
+//Update the name of the relevant quiz.
+function adminQuizNameUpdate(authUserId, quizId, name){
+  return{};
 }
