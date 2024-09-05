d18237fa6a0cda0af45e23c67284bd181d1bafe1
Guangwei Hu <z5526309@ad.unsw.edu.au>
2024-09-05 15:11:25 +1000

    chore: add the funtion adminQuizRemove

--- src/quiz.js
+++ src/quiz.js
@@ -53,3 +53,13 @@ function adminQuizCreate (authUserId, name, description) {
     quizId: 2,
   };
 }
+
+/**
+ * 
+ * @param {number} authUserId - The ID of the authenticated user.
+ * @param {srting} quizId - The ID of quiz.
+ * @returns 
+ */
+function adminQuizRemove(authUserId, quizId) {
+  return {};
+}
