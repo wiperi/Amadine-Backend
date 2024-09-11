60fd0e4f54fee6d7ff29f582aa2d3c1d1c904a58
Yibin <z5567083@ad.unsw.edu.au>
2024-09-12 00:44:46 +1000

    feat: finish invalidUserName

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,6 @@
 import { getData, setData } from "./dataStore.js";
-import { ERROR_MESSAGES } from "./errors.js";
+import { ERROR_MESSAGES } from "../src/errors.js";
 import isEmail from 'validator/lib/isEmail.js';
-
 
 export function getNewID() {
   return 42;
@@ -16,7 +15,33 @@ export function invalidEmail(email) {
 }
 
 export function invalidUserName(userName) {
+  const name_devide = userName.split(' ');
+  if (name_devide.length < 2) {
+    return { isValid: true, message: ERROR_MESSAGES.NAME_FIRST_LENGTH };
+  }
+
+  const NameFirst = name_devide[0];
+  const NameLast = name_devide.slice(1).join(' ');
+  const nameRegex = /^[a-zA-Z\s'-]+$/;
+
+  if (NameFirst.length < 2 || NameFirst.length > 20) {
+    return { isValid: true, message: ERROR_MESSAGES.NAME_FIRST_LENGTH };
+  }
+
+  if (!nameRegex.test(NameFirst)) {
+    return { isValid: true, message: ERROR_MESSAGES.NAME_FIRST_FORMAT };
+  }
+
+  if (NameLast.length < 2 || NameLast.length > 20) {
+    return { isValid: true, message: ERROR_MESSAGES.NAME_LAST_LENGTH };
+  }
+
+  if (!nameRegex.test(NameLast)) {
+    return { isValid: true, message: ERROR_MESSAGES.NAME_LAST_FORMAT };
+  }
+
   return false;
+
 }
 
 export function invalidQuizName(quizName) {
