e3c990fee7fcb7f174c034fe52c4b17e064b7924
Tianyang Chen <z5567323@ad.unsw.edu.au>
2024-09-11 20:26:05 +1000

    feat!: change dataStore.js, define data type

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,5 +1,61 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW ONLY
-let data = {};
+
+/**
+ * @typedef {{
+*   authUserId: number,
+*   email: string,
+*   password: string,
+*   nameFirst: string,
+*   nameLast: string,
+*   numSuccessfulLogins: number,
+*   numFailedPasswordsSinceLastLogin: number,
+*   oldPasswords: Array<string>,
+*   quizzes: Array<number>
+ * }} User
+ */
+
+/**
+ * @typedef {{
+*   authUserId: number,
+*   quizId: number,
+*   name: string,
+*   description: string,
+*   timeCreated: number,
+*   timeLastEdited: number,
+*   active: boolean
+* }} Quiz
+*/
+
+/**
+ * @type {{UserMap: Map<number, User>, QuizMap: Map<number, Quiz>}}
+ */
+let data = {
+  UserMap: new Map([
+    [616425961674, {
+      authUserId: 616425961674,
+      email: 'cheongmail@gmail.com',
+      password: 'nicepassword',
+      nameFirst: 'Cheong',
+      nameLast: 'Zhang',
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 4,
+      oldPasswords: ['MyOldPass1234'],
+      quizzes: [174712181430],
+    }],
+  ]),
+
+  QuizMap: new Map([
+    [174712181430, {
+      authUserId: 616425961674,
+      quizId: 174712181430,
+      name: '1093 class',
+      description: 'I love sasa',
+      timeCreated: 1627312,
+      timeLastEdited: 128372,
+      active: true,
+    }]
+  ]),
+};
 
 // YOU SHOULD MODIFY THIS OBJECT ABOVE ONLY
 
