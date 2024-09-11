4eb410c30d6c05ebfc1b0c5b63cee9b1b2f7c492
Tianyang Chen <z5567323@ad.unsw.edu.au>
2024-09-11 20:25:25 +1000

    chore: create helper.js, auth.test.js, errors.js, provided a general template for each file

---
+++ src/helper.js
@@ -0,0 +1,30 @@
+import { getData, setData } from "./dataStore.js";
+import { ERROR_MESSAGES } from "./errors.js";
+import isEmail from 'validator/lib/isEmail.js';
+
+
+export function getNewID() {
+  return 42;
+}
+
+export function invalidPassword(password) {
+  return false
+}
+
+export function invalidEmail(email) {
+  return false;
+}
+
+export function invalidUserName(userName) {
+  return false;
+}
+
+export function invalidQuizName(quizName) {
+  return false;
+}
+
+export function invalidQuizDescription(quizDescription) {
+  return false;
+}
+
+
