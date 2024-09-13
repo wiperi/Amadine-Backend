da7de6bbc27ee9b9598512ac0b50897b81c5eacc
Yuting <z5535043@ad.unsw.edu.au>
2024-09-13 10:14:05 +1000

    chore: add error message for adminAuthLogin error cases

--- src/errors.js
+++ src/errors.js
@@ -4,6 +4,8 @@ const ERROR_MESSAGES = {
   NAME_FIRST_FORMAT: 'NameFirst contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.',
   NAME_LAST_LENGTH: 'NameLast need more than 1 characters and less than 21 characters.',
   NAME_LAST_FORMAT: 'NameLast contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.',
+  EMAIL_EXISTENCE: 'Email address does not exist.',
+  PASSWORD_EXITSTENCE: 'Password is not correct for the given email.',
 }
 
 export { ERROR_MESSAGES };
