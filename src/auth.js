a721128e8b2922be18c7c11ce1899dcaadfefa51
Yuting <z5535043@ad.unsw.edu.au>
2024-09-13 20:38:42 +1000

    impr: change data and user variable from let to const

--- src/auth.js
+++ src/auth.js
@@ -55,9 +55,26 @@ export function adminAuthRegister(email, password, nameFirst, nameLast) {
  * @returns {Object} - Object with authUserId value
  */
 export function adminAuthLogin ( email, password ) {
+  const data = getData();
+
+  const user = data.user.find(user => user.email === email);
+
+  // error case 1:
+  //  email address does not exist
+  if (!user) {
+    return { error: ERROR_MESSAGES.EMAIL_EXISTENCE };
+  }
+
+  // error case 2:
+  //  password is not correct for the given email
+  if (user.password !== password) {
+    return { error: ERROR_MESSAGES.PASSWORD_EXITSTENCE};
+  }
+  
   return {
     authUserId: user.userId,
   };
+
 }
 
 /**
