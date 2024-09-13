c5ea0156993466f8c9225c780c6abde0b3f21a36
Yuting <z5535043@ad.unsw.edu.au>
2024-09-13 20:54:53 +1000

    fix: fix mispelling 'existence'

--- src/errors.js
+++ src/errors.js
@@ -2,13 +2,8 @@ const ERROR_MESSAGES = {
   INVALID_EMAIL: 'Invalid email format or duplicated email',
   INVALID_NAME: 'Invalid name format',
   INVALID_PASSWORD: 'Invalid password format',
-  EMAIL_FORMAT: 'Invalid email format',
-  NAME_FIRST_LENGTH: 'NameFirst need more than 1 characters and less than 21 characters.',
-  NAME_FIRST_FORMAT: 'NameFirst contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.',
-  NAME_LAST_LENGTH: 'NameLast need more than 1 characters and less than 21 characters.',
-  NAME_LAST_FORMAT: 'NameLast contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.',
   EMAIL_EXISTENCE: 'Email address does not exist.',
-  PASSWORD_EXITSTENCE: 'Password is not correct for the given email.',
+  PASSWORD_EXISTENCE: 'Password is not correct for the given email.',
 }
 
 export { ERROR_MESSAGES };
