5949548f036eeb104eeb7a1e1ec2193ade40317f
Tianyang Chen <z5567323@ad.unsw.edu.au>
2024-09-25 12:19:57 +1000

    chore: update .ts postfix

--- tests/echo.test.ts
+++ tests/echo.test.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 // Do not delete this file
 // Note:: None of these tests access the dataStore - this is because black box tests can only access the specification
 // If you want to access information in the dataStore, you will have to do so via a function in the specification and not via direct variable access.
