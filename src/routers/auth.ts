fd089b93592d3d782d1479e4a3439d11300ada91
Yuting <z5535043@ad.unsw.edu.au>
2024-09-28 15:41:13 +1000

    chore: add router for adminAuthLogin

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { adminAuthRegister } from '../auth';
+import { adminAuthLogin, adminAuthRegister } from '../auth';
 
 export const authRouter = Router();
 
@@ -7,6 +7,15 @@ authRouter.post('/register', (req: Request, res: Response) => {
   const { email, password, nameFirst, nameLast } = req.body;
   try {
     return res.json(adminAuthRegister(email, password, nameFirst, nameLast));
+  } catch (error) {
+    return res.status(error.statusCode).json({ error: error.message });
+  }
+});
+
+authRouter.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    return res.json(adminAuthLogin(email, password));
   } catch (error) {
     return res.status(error.statusCode).json({ error: error.message });
   }
