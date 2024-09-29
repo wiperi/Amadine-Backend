05693e5d64f730242663d8e6e52ed4f97d1c38a8
Cheong <z5512751@unsw.edu.au>
2024-09-29 10:28:58 +1000

    feat: add route for AQI and AQC

--- src/routers/quiz.ts
+++ src/routers/quiz.ts
@@ -1,7 +1,28 @@
 import { Router, Request, Response } from 'express';
-
+import { adminQuizCreate, adminQuizInfo } from '../quiz';
+import { parse } from 'path';
 export const quizRouter = Router();
 
 quizRouter.get('/', (req: Request, res: Response) => {
   return res.status(200).json({ message: 'Quiz route' });
+});
+
+quizRouter.post('/', (req: Request, res: Response) => {
+  const { authUserId, name, description } = req.body;
+  try {
+    return res.json(adminQuizCreate(authUserId, name, description));
+  } catch (error) {
+    return res.status(error.statusCode).json({ error: error.message });
+  } 
+});
+
+quizRouter.get('/:quizid', (req: Request, res: Response) => {
+  const { quizid } = req.params;
+  try {
+    const quizIdNumber = parseInt(quizid);
+    const result = adminQuizInfo(req.body.authUserId, quizIdNumber);
+    return res.json(result);
+  } catch (error) {
+    return res.status(error.statusCode).json({ error: error.message });
+  }
 });
