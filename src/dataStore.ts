3ac600a69d296b6658b6ddcb553e660955eb8f5c
Tianyang Chen <z5567323@ad.unsw.edu.au>
2024-10-16 14:14:48 +1100

    fix(loadData): reconstruct object when loadData()

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,5 +1,5 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW ONLY
-import { User, UserSession, Quiz, QuizSession, Player } from './models/Classes';
+import { User, UserSession, Quiz, QuizSession, Player, Question, Answer } from './models/Classes';
 import fs from 'fs';
 import path from 'path';
 
@@ -58,7 +58,18 @@ function setData(newData?: DataStore): void {
  * Load data from json file.
  */
 function loadData(): void {
-  data = JSON.parse(fs.readFileSync(DATA_FILE_PATH, 'utf8'));
+  let rawData: any = JSON.parse(fs.readFileSync(DATA_FILE_PATH, 'utf8'));
+
+  // Reconstruct the objects
+  rawData.users.forEach((u: any) => Object.setPrototypeOf(u, User.prototype));
+  rawData.userSessions.forEach((us: any) => Object.setPrototypeOf(us, UserSession.prototype));
+  rawData.quizzes.forEach((q: any) => Object.setPrototypeOf(q, Quiz.prototype)
+    && q.questions.forEach((q: any) => Object.setPrototypeOf(q, Question.prototype)
+      && q.answers.forEach((a: any) => Object.setPrototypeOf(a, Answer.prototype))));
+  rawData.quizSessions.forEach((qs: any) => Object.setPrototypeOf(qs, QuizSession.prototype));
+  rawData.players.forEach((p: any) => Object.setPrototypeOf(p, Player.prototype));
+
+  data = rawData;
 }
 
 export { getData, setData, loadData };
