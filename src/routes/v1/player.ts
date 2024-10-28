8851be958c513c00ad0a895772595f1047bb98aa
Guangwei Hu <z5526309@ad.unsw.edu.au>
2024-10-28 14:35:51 +1100

    feat: add the route for playerGetStatusInSession

--- src/routes/v1/player.ts
+++ src/routes/v1/player.ts
@@ -5,6 +5,7 @@ import {
   playerPostMessage,
   adminPlayerSubmitAnswers,
   playerGetMessage,
+  playerGetSessionStatus,
 } from '@/services/player';
 import { tryCatch } from '@/utils/helper';
 
@@ -44,5 +45,10 @@ router.get('/:playerid(-?\\d+)/chat', (req: Request, res: Response, next: NextFu
   const playerId = parseInt(req.params.playerid);
   tryCatch(() => playerGetMessage(playerId), req, res, next);
 });
+
+router.get('/:playerid(-?\\d+)', (req: Request, res: Response, next: NextFunction) => {
+  const playerId = parseInt(req.params.playerid);
+  tryCatch(() => playerGetSessionStatus(playerId), req, res, next);
+})
 
 export default router;
